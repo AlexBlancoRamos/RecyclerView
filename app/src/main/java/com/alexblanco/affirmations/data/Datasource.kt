aa4dc95373e97af5e271d3442c8f2b34a3acf861
AlexBlancoRamos <alex.blanco@institutvidreres.cat>
2023-11-06 09:12:00 +0100

    Initial commit

---
+++ app/src/main/java/com/alexblanco/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.alexblanco.affirmations.data
+
+import com.alexblanco.affirmations.R
+import com.alexblanco.affirmations.model.Affirmation
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/alexblanco/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.alexblanco.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+}
